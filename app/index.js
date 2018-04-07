934c5bc36085d782bd65e7f3088a0da03f7cb6ea
Will Kim <rlagmlckd@gmail.com>
2018-04-06 21:22:44 -0500

    Added more jQuery to section01

--- app/index.js
+++ app/index.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-    // all custom jQuery will go here
-    $(".container-01").click(function() {
-      alert("Click!");
+    // Section 01
+    $("#section01-cult").click(function() {
+      $("#section01-cult").addClass("section01-cult");
     });
+    $("#section01-of").click(function() {
+      $("#section01-of").addClass("section01-of");
+    });
+    $("#section01-the").click(function() {
+      $("#section01-the").addClass("section01-the");
+    });
+    $("#section01-ugly").click(function() {
+      $("#section01-ugly").addClass("section01-ugly");
+    });
+
+
 });
