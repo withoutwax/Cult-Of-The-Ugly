1be9698b4ef2f116ec00a0320ad428458c853c63
Will Kim <rlagmlckd@gmail.com>
2018-05-11 18:58:01 -0500

    Implementing React-Router

--- nightcap/src/App.js
+++ nightcap/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/Home';
@@ -7,10 +8,14 @@ import Header from './shared/Header';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+
+          <Route exact path="/" component={Home} />
+
+        </div>
+      </Router>
     );
   }
 }
