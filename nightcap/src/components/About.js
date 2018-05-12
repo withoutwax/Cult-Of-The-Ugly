f3a256a036e1bac3a28f38855c09dcf0f84e9753
Will Kim <rlagmlckd@gmail.com>
2018-05-11 20:47:09 -0500

    Implemented RSVP & About

--- nightcap/src/components/About.js
+++ nightcap/src/components/About.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './About.css';
 
+import mig_reyes from '../assets/about/mig-reyes.png';
+import amy_shwartz from '../assets/about/amy-shwartz.png';
+import eileen_tjan from '../assets/about/eileen-tjan.png';
+import aiga_chicago from '../assets/about/aiga-chicago.png';
+
 class About extends Component {
   render() {
     return (
@@ -24,7 +29,10 @@ class About extends Component {
             <h2>tony junior</h2>
           </div>
           <div className="about-section-03">
-            ** Photos of people **
+            <img src={mig_reyes} />
+            <img src={amy_shwartz} />
+            <img src={eileen_tjan} />
+            <img src={aiga_chicago} />
           </div>
         </section>
       </div>
